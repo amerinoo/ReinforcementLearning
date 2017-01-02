e15a77fe4ff3a4c6c569bb06841133dbae741834
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2017-01-03 00:52:20 +0100

    Pass Q2

--- analysis.py
+++ analysis.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -20,8 +20,11 @@
 # value iteration.
 
 def question2():
-    answerDiscount = 0.9
-    answerNoise = 0.2
+    answerDiscount = 0.9 # No s'ha canviat
+
+    ''' No volem que prengui decicions dolentes, aquest parametre es un factor
+    per decidir si agafa o no una decicio dolenta.'''
+    answerNoise = 0.0
     return answerDiscount, answerNoise
 
 def question3a():
